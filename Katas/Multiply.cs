2b768d0e6ffe4eb667963672c004be31b31ab143
Vincent Meijer <vincent.meijer@movares.nl>
2020-02-07 10:23:20 +0100

    Add Multiply Kata

---
+++ Katas/Multiply.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Katas
+{
+    public class CustomMath
+    {
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            const int expected = 6;
+            var actual = CustomMath.multiply(2, 3);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
