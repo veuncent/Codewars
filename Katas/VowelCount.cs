5c20accbf60411832df7d1402ca256759621a6b1
Vincent Meijer <vincent.meijer@movares.nl>
2020-02-12 10:02:45 +0100

    Add Vowel Count kata

---
+++ Katas/VowelCount.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Katas
+{
+    public static class VowelCount
+    {
+        public static int GetVowelCount(string str)
+        {
+            var vowels = new List<string> {"a", "e", "i", "o", "u"};
+            
+            return str.Count(c => vowels.Contains(c.ToString()));
+        }
+    }
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void TestCase1()
+        {
+            Assert.AreEqual(5, VowelCount.GetVowelCount("abracadabra"), "Nope!");
+        }
+    }
+}
