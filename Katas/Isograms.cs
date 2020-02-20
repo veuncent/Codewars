5b4fdfd83ba8839ffbe42dae6400f25ec934ab26
Vincent Meijer <vincent.meijer@movares.nl>
2020-02-20 09:17:12 +0100

    Add Isograms Kata

---
+++ Katas/Isograms.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Katas
+{
+    public class Isograms
+    {
+        public static bool IsIsogram(string str)
+        {
+            var groups = str
+                .ToLower()
+                .GroupBy(c => c);
+
+            return ! groups
+                .Any(g => g.Count() > 1);
+        }
+    }
+
+    [TestFixture]
+    public class BasicTests
+    {
+
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Dermatoglyphics").Returns(true);
+                yield return new TestCaseData("isogram").Returns(true);
+                yield return new TestCaseData("moose").Returns(false);
+                yield return new TestCaseData("isIsogram").Returns(false);
+                yield return new TestCaseData("aba").Returns(false);
+                yield return new TestCaseData("moOse").Returns(false);
+                yield return new TestCaseData("thumbscrewjapingly").Returns(true);
+                yield return new TestCaseData("").Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => Isograms.IsIsogram(str);
+    }
+}
